bc032fee24c00aaa403af4ab9b9d9dc8d37fc02d
Muhammad Awais <64267313+Awais-mohammad@users.noreply.github.com>
2024-07-03 23:24:21 +0500

    Add project files.

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using morleys_backend.Models.DbModels;
+
+namespace morleys_backend.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
---
+++ Models/DbModels/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace morleys_backend.Models.DbModels
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
